66ca7b1d3ca9e91e54d6b40810f6503ed26bfad3
Solaiman El Bacha <48126212+Solaiman-MultiSafepay@users.noreply.github.com>
2019-09-19 16:27:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ ! -f ./slack-project/spellcheck.yaml ]; then
-    cp /spellcheck.yaml .
+    cp slack-project/spellcheck.yaml .
 fi
 
 if [ ! -f ./slack-project/wordlist.txt ]; then
-    cp /wordlist.txt .
+    cp slack-project/wordlist.txt .
 fi
 
 pyspelling -c spellcheck.yaml
